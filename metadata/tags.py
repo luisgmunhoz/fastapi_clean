392685f3ba86e4c8e15b3676c87b527ad7f61fc4
luisgmunhoz <luisguilherme.sm@hotmail.com>
2024-12-29 15:24:57 -0300

    chore: remove author and books

--- metadata/tags.py
+++ metadata/tags.py
@@ -1,10 +1,1 @@
-Tags = [
-    {
-        "name": "author",
-        "description": "Contains CRUD operation on Authors",
-    },
-    {
-        "name": "book",
-        "description": "Contains CRUD operation on Books",
-    },
-]
+Tags: list[dict[str, str]] = []

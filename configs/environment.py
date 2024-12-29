051a30e9acc690b2eb2979a72e52fcffaf7e9c82
luisgmunhoz <luisguilherme.sm@hotmail.com>
2024-12-29 16:07:58 -0300

    chore: add new env variable

--- configs/environment.py
+++ configs/environment.py
@@ -11,15 +11,16 @@ def get_env_filename() -> str:
 
 
 class EnvironmentSettings(BaseSettings):
-    API_VERSION: str | None
-    APP_NAME: str | None
-    DATABASE_DIALECT: str | None
-    DATABASE_HOSTNAME: str | None
-    DATABASE_NAME: str | None
-    DATABASE_PASSWORD: str | None
-    DATABASE_PORT: int | None
-    DATABASE_USERNAME: str | None
-    DEBUG_MODE: bool | None
+    API_VERSION: str
+    APP_NAME: str
+    DATABASE_DIALECT: str
+    DATABASE_HOSTNAME: str
+    DATABASE_NAME: str
+    DATABASE_PASSWORD: str
+    DATABASE_PORT: int
+    DATABASE_USERNAME: str
+    DEBUG_MODE: bool
+    NEXTAUTH_SECRET: str
     ENV: str
 
     class Config:

0ea26fecbc19a23c1f0fd598d6a93d4ac0e9e5eb
luisgmunhoz <luisguilherme.sm@hotmail.com>
2024-12-29 15:23:49 -0300

    chore: remove author and books

--- schemas/graphql/query.py
+++ schemas/graphql/query.py
@@ -1,29 +1,6 @@
 import strawberry
-from strawberry.types import Info
-
-from configs.graphql import get_author_service, get_book_service
-from schemas.graphql.author import BookSchema
-from schemas.graphql.author import AuthorSchema
 
 
 @strawberry.type(description="Query all entities")
 class Query:
-    @strawberry.field(description="Get an Author")
-    def author(self, id: int, info: Info) -> AuthorSchema:
-        author_service = get_author_service(info)
-        return author_service.get(id)
-
-    @strawberry.field(description="List all Authors")
-    def authors(self, info: Info) -> list[AuthorSchema]:
-        author_service = get_author_service(info)
-        return author_service.list_items()
-
-    @strawberry.field(description="Get a Book")
-    def book(self, id: int, info: Info) -> BookSchema:
-        book_service = get_book_service(info)
-        return book_service.get(id)
-
-    @strawberry.field(description="List all Books")
-    def books(self, info: Info) -> list[BookSchema]:
-        book_service = get_book_service(info)
-        return book_service.list_items()
+    pass

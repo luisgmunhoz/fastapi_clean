a41863c831d739b7b7497cd2f3b72cab5e506340
luisgmunhoz <luisguilherme.sm@hotmail.com>
2024-12-29 15:22:33 -0300

    lint

--- configs/database.py
+++ configs/database.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from collections.abc import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, scoped_session, sessionmaker
@@ -11,7 +11,7 @@ if env.ENV == "test":
     DATABASE_URL = "sqlite:///./sql_app.db"
 else:
     # Generate Database URL
-    DATABASE_URL = f"{env.DATABASE_DIALECT}://{env.DATABASE_USERNAME}:{env.DATABASE_PASSWORD}@{env.DATABASE_HOSTNAME}:{env.DATABASE_PORT}/{env.DATABASE_NAME}"  # noqa
+    DATABASE_URL = f"{env.DATABASE_DIALECT}://{env.DATABASE_USERNAME}:{env.DATABASE_PASSWORD}@{env.DATABASE_HOSTNAME}:{env.DATABASE_PORT}/{env.DATABASE_NAME}"
 
 # Create Database Engine
 engine = create_engine(DATABASE_URL, echo=env.DEBUG_MODE, future=True)
